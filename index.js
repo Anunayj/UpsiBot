54a0d35efa3fa89bfa8976abb3ed918f2b2ac76a
Anunayj <anunay.j1@gmail.com>
2020-08-26 23:27:28 +0530

    That guild bug thingy, Discord really sucks

--- index.js
+++ index.js
@@ -780,7 +780,7 @@ async function guildStats(msg = new Eris.Message(), args) {
         }
     }
     embed.title(`${tempGuild.name} - ${Object.keys(tempGuild.members).length} Members`);
-    embed.field(`Guild Description`, `${tempGuild.description}`);
+    embed.field(`Guild Description`, `${tempGuild.description.length ? tempGuild.description : "None Set"}`);
     embed.field(`Guild Master`, `${gm}`);
     embed.field(`Guild EXP`, `${tempGuild.exp}`);
     embed.field(`Members Online`, `${tempGuild.achievements.ONLINE_PLAYERS}`);
