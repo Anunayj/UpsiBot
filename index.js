5dc783bfc4dd1d0aaabf7dcc47f80146fce0313b
Anunay Jain <anunay.j1@gmail.com>
2020-08-31 16:56:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -586,7 +586,7 @@ class splashNotifier {
                 setTimeout(function(){
                     delete this.duplicate[hasEmbed.title];
                     console.log(`Deleted ${hasEmbed.title}`);
-                }.bind(this),30000)
+                }.bind(this),90000)
                 leechserver.publish({
                     type: (hasEmbed.title.match(/(party|p) join \w+/i) ? "party" : "hub"),
                     place: hasEmbed.title,
